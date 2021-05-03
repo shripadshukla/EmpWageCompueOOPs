177e116cdce8fddfe86327305640c41ee3e9a702
shripadshukla <shripadshukla46@gmail.com>
2021-05-03 12:10:01 +0530

    UC5: Wage Calculation Maximum for 100 days added

--- empWageCompue/EmpWageCompute.java
+++ empWageCompue/EmpWageCompute.java
@@ -2,43 +2,44 @@ package empWageCompue;
 
 public class EmpWageCompute {
 	
-	int Employee;						//declaring all variables
+	int Full_time_employee;					//declaring all variables
+	int Part_time_employee;
 	int Full_time_Wage;
 	int Full_time_Hour;
 	int Part_time_Hour;
 	
 	// Total working days for a month
 	int Working_days;
+	int n;
+	int d;
 	
 	public EmpWageCompute() {        	// Making constructor which helps to not create object again and again
 		
-		Employee = 1;					// assign values in the constructor 
-		Full_time_Wage = 20;			
+		Full_time_employee = 1;					// assign values in the constructor 
+		Part_time_employee = 2;
+		Full_time_Wage = 20;
 		Full_time_Hour = 8;
 		Part_time_Hour = 4;
-		
 		Working_days = 20;
-		
+		n = 100;
+		d = 20;
 	}
 	
 	public static void main(String []args) {
 	
-		EmpWageCompute UC4 = new EmpWageCompute();		//object created
+		EmpWageCompute UC6 = new EmpWageCompute();		//object created
 		
-		switch (UC4.Employee) {								//now calling object reference
-		
-		case 1:
-			System.out.println("Employee is Full Time Present");
+			for(int i=1; i<=(UC6.n); i++) {
 			
-			double Emp_Wage1 = (UC4.Working_days * UC4.Full_time_Wage * UC4.Full_time_Hour);
-			System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
-		
-		default:
-			System.out.println("Employee is Part Time Present");
-			
-			double Emp_Wage2 = (UC4.Working_days * UC4.Full_time_Wage * UC4.Part_time_Hour);
-			System.out.println("Part Time Employee Wage for a month is = " +Emp_Wage2);	
-	
+			for (int j=1; j<=(UC6.d); j++) {
+				
+				double Emp_Wage1 = (j * UC6.Full_time_Wage * i);
+				double Emp_Wage2 = (j * UC6.Full_time_Wage/2 * i);
+				
+				System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
+				System.out.println("Part Time Employee Wage for a month is = " +Emp_Wage2 );
+				
+			}
 		}
 	}
 }
