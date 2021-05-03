f9ec44a937d176d01874117d363fbf0819dfd3aa
shripadshukla <shripadshukla46@gmail.com>
2021-05-03 11:49:28 +0530

    UC2: Wage calculation added

--- empWageCompue/EmpWageCompute.java
+++ empWageCompue/EmpWageCompute.java
@@ -2,22 +2,21 @@ package empWageCompue;
 
 public class EmpWageCompute {
 	
-	public static int IS_PRESENT = 1;
+	public static void Earning() {
+		
+		 int Wage_Per_Hour = 20;
+		 int Full_day_Hour = 8;
+		
+		 double EmpdailyWage = (Wage_Per_Hour * Full_day_Hour);
+		 System.out.println("Employee daily wage is = "+EmpdailyWage);
+		
+	}
 	
-	public static double empCheck = Math.floor(Math.random() * 10) % 2;
-
 	
 	public static void main(String[] args) {
-
-
-		if (empCheck == IS_PRESENT)
-			
-			System.out.println("Employee is present");
 		
-		else
-			
-			System.out.println("Employee is absent");
-	
+		Earning();
+
 	}
 
 }
