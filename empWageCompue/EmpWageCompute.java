c0cfb6ae3759deb55a07abcd9fd98505fa9d0d81
shripadshukla <shripadshukla46@gmail.com>
2021-05-03 11:53:18 +0530

    UC3: Part time wage calculation added.

--- empWageCompue/EmpWageCompute.java
+++ empWageCompue/EmpWageCompute.java
@@ -4,19 +4,25 @@ public class EmpWageCompute {
 	
 	public static void Earning() {
 		
-		 int Wage_Per_Hour = 20;
-		 int Full_day_Hour = 8;
-		
-		 double EmpdailyWage = (Wage_Per_Hour * Full_day_Hour);
-		 System.out.println("Employee daily wage is = "+EmpdailyWage);
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		Earning();
+		int Full_time_Employee = 1;
+		int Part_time_Employee = 2;
+		int Wage_Per_Hour = 20;
+		int Full_day_Hour = 8;
+		int Part_time_Hour =4;
 
-	}
+			
+				double Full_time_EmpdailyWage = (Wage_Per_Hour * Full_day_Hour);
+		
+				double Part_time_EmpdailyWage = (Wage_Per_Hour * Part_time_Hour);
+			
+				System.out.println("Full Time Employee daily wage is = "+Full_time_EmpdailyWage);
+	
+				System.out.println("Part Time Employee daily wage is = "+ Part_time_EmpdailyWage);
+		
+				}
+					public static void main(String[] args) {
+		
+						Earning();		
 
+			}	
 }
