401170fa24427c4de1cc00e17d0f0264c8f1fffe
shripadshukla <shripadshukla46@gmail.com>
2021-05-03 12:14:21 +0530

    UC6: Method calling added

--- empWageCompue/EmpWageCompute.java
+++ empWageCompue/EmpWageCompute.java
@@ -2,44 +2,25 @@ package empWageCompue;
 
 public class EmpWageCompute {
 	
-	int Full_time_employee;					//declaring all variables
-	int Part_time_employee;
-	int Full_time_Wage;
-	int Full_time_Hour;
-	int Part_time_Hour;
+		static void mymethod (int Full_time_employee, int Part_time_employee, int Full_time_Wage,
+				int Full_time_Hour, int Part_time_Hour, int Working_days,  int n , int d ) {
+
+				for(int i=1; i<=n; i++) {
+
+						for (int j=1; j<=d; j++) {
+
+								double Emp_Wage1 = (j * Full_time_Wage * i);
+								double Emp_Wage2 = (j * Full_time_Wage/2 * i);
+
+								System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
+								System.out.println("Part Time Employee Wage for a month is = " +Emp_Wage2 );
+						}
+				}
+		}	
+
+		public static void main(String []args) {
+
+			mymethod(1, 2, 20, 8, 4, 20, 100, 20);		//object created
 	
-	// Total working days for a month
-	int Working_days;
-	int n;
-	int d;
-	
-	public EmpWageCompute() {        	// Making constructor which helps to not create object again and again
-		
-		Full_time_employee = 1;					// assign values in the constructor 
-		Part_time_employee = 2;
-		Full_time_Wage = 20;
-		Full_time_Hour = 8;
-		Part_time_Hour = 4;
-		Working_days = 20;
-		n = 100;
-		d = 20;
-	}
-	
-	public static void main(String []args) {
-	
-		EmpWageCompute UC6 = new EmpWageCompute();		//object created
-		
-			for(int i=1; i<=(UC6.n); i++) {
-			
-			for (int j=1; j<=(UC6.d); j++) {
-				
-				double Emp_Wage1 = (j * UC6.Full_time_Wage * i);
-				double Emp_Wage2 = (j * UC6.Full_time_Wage/2 * i);
-				
-				System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
-				System.out.println("Part Time Employee Wage for a month is = " +Emp_Wage2 );
-				
-			}
 		}
-	}
 }
